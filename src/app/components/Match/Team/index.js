6b97549a94219058414080e48e345620fa1cae80
Rafael Vieira <rafaellvs322@gmail.com>
2020-04-12 19:50:33 -0300

    ENH: break Match into smaller components

---
+++ src/app/components/Match/Team/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { getHeroImage } from 'app/helpers/utils'
+
+import Image from 'app/components/core/Image'
+
+import { Container } from './styled'
+
+const Team = ({ heroes }) => {
+  return (
+    <Container>
+      {
+        heroes.map(id =>
+          <Image
+            key={id}
+            src={getHeroImage(id)}
+          />
+        )
+      }
+    </Container>
+  )
+}
+
+Team.propTypes = {
+  heroes: PropTypes.array
+}
+
+export default Team
--- src/app/components/Match/styled.js
+++ src/app/components/Match/styled.js
@@ -1,16 +1,12 @@
 import styled from 'styled-components'
+
 import theme from 'app/helpers/theme'
 
-export const FullMatch = styled.div`
+export const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   background: ${theme.colors.background};
-  height: 100%;
   padding: 3rem;
-`
-
-export const Heroes = styled.div`
-  display: flex;
 `
