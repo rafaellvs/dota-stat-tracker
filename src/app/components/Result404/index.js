fdc92858cd0dfbc7732347abea3966af6226526e
Rafael Vieira <rafaellvs322@gmail.com>
2020-04-12 19:46:08 -0300

    FEA: add Result404 component

---
+++ src/app/components/Result404/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Text from 'app/components/core/Text'
+
+import { Container } from './styled'
+
+const Result404 = () =>
+  <Container>
+    <Text component='h2'>
+      found nothing :(
+    </Text>
+  </Container>
+
+export default Result404
