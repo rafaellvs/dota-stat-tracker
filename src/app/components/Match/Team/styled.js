b9bbdea83692e839363d3a2ba02e9bac770391dd
Rafael Vieira <rafaellvs322@gmail.com>
2020-04-14 18:27:25 -0300

    FEA: add data to Match component

--- src/app/components/Match/Team/styled.js
+++ src/app/components/Match/Team/styled.js
@@ -1,5 +1,65 @@
 import styled from 'styled-components'
 
+import theme from 'app/helpers/theme'
+
+import RouterLink from 'app/components/core/RouterLink'
+import Text from 'app/components/core/Text'
+
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 1.5rem 0;
+`
+
+export const Hero = styled.div`
+  display: flex;
+  align-items: center;
+  padding: .3rem 0;
+  transition: background .3s;
+
+  & *:not(img) {
+    flex: 1;
+  }
+
+  &:hover {
+    background: ${theme.colors.hover};
+  }
+`
+
+export const PlayerLink = styled(RouterLink)`
+  text-decoration: none;
+  color: inherit;
+  transition: color .3s;
+
+  &:hover{
+    color: ${theme.colors.hover};
+  }
+`
+
+export const PlayerName = styled(Text)`
+  color: ${({ color }) => color};
+  font-weight: bold;
+`
+
+export const Items = styled.div``
+
+export const Inventory = styled.div`
+  display: flex;
+  min-width: 240px;
+  min-height: 30px;
+
+  & img {
+    width: 40px;
+  }
+`
+
+export const Backpack = styled.div`
+  display: flex;
+  min-width: 120px;
+  min-height: 25px;
+
+  & img {
+    width: 30px;
+  }
 `
--- src/app/components/Match/index.js
+++ src/app/components/Match/index.js
@@ -2,14 +2,21 @@ import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import PropTypes from 'prop-types'
 
-import { isEmpty, getRadiantHeroes, getDireHeroes } from 'app/helpers/utils'
+import {
+  isEmpty,
+  getRadiantPlayers,
+  getDirePlayers,
+  getGameDuration,
+  getGameMode,
+  getLobbyType,
+} from 'app/helpers/utils'
 import { fetchSelectedMatch } from 'app/redux/actions/matches'
 
 import Text from 'app/components/core/Text'
 
 import Team from './Team'
 
-import { Container } from './styled'
+import { Container, Teams } from './styled'
 
 const Match = ({ id }) => {
   const dispatch = useDispatch()
@@ -21,15 +28,24 @@ const Match = ({ id }) => {
 
   return !isEmpty(match) &&
     <Container>
-      <Text component='h2'>
+      <Text component='h1'>
         Match {match.match_id}
       </Text>
 
-      <Text component='h4'>Radiant:</Text>
-      <Team heroes={getRadiantHeroes(match)} />
+      <>
+        <Text component='h2'>{`${match.radiant_score} - ${match.dire_score}`}</Text>
+        <Text>{getGameDuration(match.duration)}</Text>
+        <Text>{getGameMode(match.game_mode)}</Text>
+        <Text>{getLobbyType(match.lobby_type)}</Text>
+      </>
 
-      <Text component='h4'>Dire:</Text>
-      <Team heroes={getDireHeroes(match)} />
+      <Teams>
+        <Text component='h4'>Radiant:</Text>
+        <Team players={getRadiantPlayers(match.players)} />
+
+        <Text component='h4'>Dire:</Text>
+        <Team players={getDirePlayers(match.players)} />
+      </Teams>
     </Container>
 }
 
