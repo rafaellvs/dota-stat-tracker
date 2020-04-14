0671d387fce4c1603f6733b7ee4c3a41e5151675
Rafael Vieira <rafaellvs322@gmail.com>
2020-04-14 18:22:18 -0300

    ENH: mkdir constants folder

---
+++ src/app/helpers/constants/lobbies.js
@@ -0,0 +1,61 @@
+const lobies = [
+  {
+    id: 0,
+    slug: 'lobby_type_normal',
+    name: 'Normal',
+    balanced: true,
+  },
+  {
+    id: 1,
+    slug: 'lobby_type_practice',
+    name: 'Practice',
+    balanced: true,
+  },
+  {
+    id: 2,
+    slug: 'lobby_type_tournament',
+    name: 'Tournament',
+    balanced: true,
+  },
+  {
+    id: 3,
+    slug: 'lobby_type_tutorial',
+    name: 'Tutorial',
+  },
+  {
+    id: 4,
+    slug: 'lobby_type_coop_bots',
+    name: 'Coop Bots',
+  },
+  {
+    id: 5,
+    slug: 'lobby_type_ranked_team_mm',
+    name: 'Ranked Team',
+    balanced: true,
+  },
+  {
+    id: 6,
+    slug: 'lobby_type_ranked_solo_mm',
+    name: 'Ranked Solo',
+    balanced: true,
+  },
+  {
+    id: 7,
+    slug: 'lobby_type_ranked',
+    name: 'Ranked',
+    balanced: true,
+  },
+  {
+    id: 8,
+    slug: 'lobby_type_1v1_mid',
+    name: '1v1 Mid',
+  },
+  {
+    id: 9,
+    slug: 'lobby_type_battle_cup',
+    name: 'Battle Cup',
+    balanced: true,
+  },
+]
+
+export default lobies
---
+++ src/app/helpers/constants/slotColors.js
@@ -0,0 +1,44 @@
+const slotColors = [
+  {
+    id: 0,
+    color: '#3074F9'
+  },
+  {
+    id: 1,
+    color: '#66FFC0'
+  },
+  {
+    id: 2,
+    color: '#BD00B7'
+  },
+  {
+    id: 3,
+    color: '#F8F50A'
+  },
+  {
+    id: 4,
+    color: '#FF6901'
+  },
+  {
+    id: 128,
+    color: '#FF88C5'
+  },
+  {
+    id: 129,
+    color: '#A2B349'
+  },
+  {
+    id: 130,
+    color: '#63DAFA'
+  },
+  {
+    id: 131,
+    color: '#01831F'
+  },
+  {
+    id: 132,
+    color: '#9F6B00'
+  },
+]
+
+export default slotColors
