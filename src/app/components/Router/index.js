86bda5bd2722d50a77da00b58fb54dfcc42cb637
Rafael Vieira <rafaellvs322@gmail.com>
2020-06-18 17:50:15 -0300

    ENH: update Router

--- src/app/components/Router/index.js
+++ src/app/components/Router/index.js
@@ -7,7 +7,11 @@ import NavBar from 'app/components/NavBar'
 import Home from 'app/pages/Home'
 import FindMatchup from 'app/pages/FindMatchup'
 import SearchResults from 'app/pages/SearchResults'
-import PlayerProfile from 'app/components/PlayerProfile'
+import Player from 'app/pages/Player'
+import PlayerIndex from 'app/components/PlayerProfile/Index'
+import PlayerMatches from 'app/pages/Player/Matches'
+import PlayerHeroes from 'app/pages/Player/Heroes'
+import PlayerPeers from 'app/pages/Player/Peers'
 import Matches from 'app/pages/Matches'
 import Match from 'app/components/Match'
 import Teams from 'app/pages/Teams'
@@ -15,7 +19,11 @@ import Team from 'app/components/Team'
 
 import loading from 'assets/images/loading.gif'
 
-import { Section, MainContent, Loading } from './styled'
+import {
+  Section,
+  MainContent,
+  Loading,
+} from './styled'
 
 const AppRouter = () => {
   const isFetching = useFetching()
@@ -37,7 +45,12 @@ const AppRouter = () => {
 
           <SearchResults path='search/:query/:input' />
 
-          <PlayerProfile path='player/:id' />
+          <Player path='/players/:id'>
+            <PlayerIndex path='/' />
+            <PlayerMatches path='matches' />
+            <PlayerHeroes path='heroes' />
+            <PlayerPeers path='peers' />
+          </Player>
 
           <Matches path='matches' />
           <Match path='match/:id' />
