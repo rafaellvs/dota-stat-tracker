85bca77dec49e28146712b1aa0bb60b60c926f64
Rafael Vieira <rafaellvs322@gmail.com>
2020-06-19 18:17:12 -0300

    FEA: add basic 404 page

---
+++ src/app/pages/Page404/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Text from 'app/components/core/Text'
+
+import { Container } from './styled'
+
+const Page404 = () =>
+  <Container>
+    <Text component='h1'>
+      404 :(
+    </Text>
+  </Container>
+
+export default Page404
---
+++ src/app/pages/Page404/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+import theme from 'app/helpers/theme'
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 6rem;
+  background: ${theme.colors.background};
+`
