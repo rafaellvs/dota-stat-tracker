a763a83b799f1a517db6eb2beda7950b78a4d504
Rafael Vieira <rafaellvs322@gmail.com>
2020-06-19 17:53:10 -0300

    ENH: update core RouterLink styles

--- src/app/components/core/RouterLink/index.js
+++ src/app/components/core/RouterLink/index.js
@@ -1,6 +1,16 @@
 import styled from 'styled-components'
 import { Link } from '@reach/router'
 
-const RouterLink = styled(Link)``
+import theme from 'app/helpers/theme'
+
+const RouterLink = styled(Link)`
+  color: ${theme.colors.white};
+  transition: color .3s;
+
+  &:hover {
+    color: ${theme.colors.hover};
+    text-decoration: none;
+  }
+`
 
 export default RouterLink
