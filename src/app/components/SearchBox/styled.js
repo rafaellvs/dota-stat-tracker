dfb5b03b2fc92822832c229cb0983286da274045
Rafael Vieira <rafaellvs322@gmail.com>
2020-06-21 20:21:00 -0300

    FIX: apply new boxShadow variants

--- src/app/components/SearchBox/styled.js
+++ src/app/components/SearchBox/styled.js
@@ -11,7 +11,7 @@ export const Container = styled.div`
   width: 100%;
   padding: 3rem;
   background: ${theme.colors.background};
-  box-shadow: ${theme.boxShadow};
+  box-shadow: ${theme.boxShadow.noBreadcrumbs};
 `
 
 export const Form = styled(CoreForm)`
