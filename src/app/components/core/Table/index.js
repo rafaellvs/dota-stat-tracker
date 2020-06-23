5806e853c310817c73f1bc1d0009c3a29e429e65
Rafael Vieira <rafaellvs322@gmail.com>
2020-06-23 03:47:24 -0300

    FEA: add Tooltip to core Table

--- src/app/components/core/Table/index.js
+++ src/app/components/core/Table/index.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import tooltipContent from 'app/helpers/tooltips'
+
+import Tooltip from 'app/components/core/Tooltip'
+
 import { CoreTable } from './styled'
+
+const findTooltip = (column) => {
+  const found =
+    tooltipContent.find(tooltip => tooltip.label === column)
+
+  return found ? found.content : ''
+}
 
 const Table = ({ columns, children }) => {
   const renderColumns = () =>
     columns.map(column =>
       <th key={column}>
-        {column}
+        <Tooltip content={findTooltip(column)}>
+          {column}
+        </Tooltip>
       </th>
     )
 
