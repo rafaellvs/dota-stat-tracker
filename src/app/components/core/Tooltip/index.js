da5b0a6b9600fb4b75375f2ebeed3b592d239ea4
Rafael Vieira <rafaellvs322@gmail.com>
2020-06-23 03:53:12 -0300

    FIX: tooltip only rendered if there is content for it

--- src/app/components/core/Tooltip/index.js
+++ src/app/components/core/Tooltip/index.js
@@ -6,9 +6,12 @@ import { Container, TooltipContainer } from './styled'
 const Tooltip = ({ content, children }) => {
   return (
     <Container>
-      <TooltipContainer>
-        <span>{content}</span>
-      </TooltipContainer>
+      {
+        content &&
+          <TooltipContainer>
+            <span>{content}</span>
+          </TooltipContainer>
+      }
 
       {children}
     </Container>
