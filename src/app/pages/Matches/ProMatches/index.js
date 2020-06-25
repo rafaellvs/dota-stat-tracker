54af13a663b6afe06a36b73de4dd70a1688140f8
Rafael Vieira <rafaellvs322@gmail.com>
2020-06-25 19:56:16 -0300

    ENH: Matches pages fetch individually

--- src/app/pages/Matches/ProMatches/index.js
+++ src/app/pages/Matches/ProMatches/index.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { fetchProMatches } from 'app/redux/actions/matches'
+
+import { isEmpty } from 'app/helpers/utils'
 
 import ProMatches from 'app/components/Matches/ProMatches'
 
-const ProMatchesPage = () =>
-  <ProMatches />
+const ProMatchesPage = () => {
+  const dispatch = useDispatch()
+  const matches = useSelector(state => state.matches.items.proMatches)
+
+  useEffect(() => {
+    dispatch(fetchProMatches())
+  }, [])
+
+  return !isEmpty(matches) &&
+    <ProMatches />
+}
 
 export default ProMatchesPage
