c02e30edc9b7c642d102a1d1f693d6fb504780ee
Rafael Vieira <rafaellvs322@gmail.com>
2020-06-25 19:49:16 -0300

    ENH: improve core Table styles

--- src/app/components/core/Table/Cell/index.js
+++ src/app/components/core/Table/Cell/index.js
@@ -1,7 +1,16 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+import theme from 'app/helpers/theme'
 
 const Cell = styled.td`
-  width: ${({ width }) => width || 'auto'};
+  max-width: ${({ width }) => width || 'unset'};
+  
+  ${
+    ({ variant }) => variant &&
+      css`
+        ${theme.variants[variant]}
+      `
+  }
 `
 
 export default Cell
