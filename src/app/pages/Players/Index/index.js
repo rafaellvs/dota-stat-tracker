cd6a3a323abc16174ff011f7df334350be6581b8
Rafael Vieira <rafaellvs322@gmail.com>
2020-06-26 05:00:32 -0300

    ENH: players index page do the fetching

--- src/app/pages/Players/Index/index.js
+++ src/app/pages/Players/Index/index.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { fetchProPlayers } from 'app/redux/actions/players'
+
+import { isEmpty } from 'app/helpers/utils'
 
 import PlayersIndex from 'app/components/Players/Index'
 
-const Index = () =>
-  <PlayersIndex />
+const Index = () => {
+  const dispatch = useDispatch()
+  const players = useSelector(state => state.players.items)
+
+  useEffect(() => {
+    dispatch(fetchProPlayers())
+  }, [])
+
+  return !isEmpty(players) &&
+    <PlayersIndex />
+}
 
 export default Index
