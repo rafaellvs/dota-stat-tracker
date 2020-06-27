f6b4a31bd7b6bbfab373bfb181135b5eda0afde9
Rafael Vieira <rafaellvs322@gmail.com>
2020-06-26 23:27:51 -0300

    ENH: improve search pages

--- src/app/pages/FindMatchup/index.js
+++ src/app/pages/FindMatchup/index.js
@@ -2,11 +2,7 @@ import React from 'react'
 
 import MatchupFinder from 'app/components/MatchupFinder'
 
-import { Container } from './styled'
-
 const FindMatchup = () =>
-  <Container>
-    <MatchupFinder />
-  </Container>
+  <MatchupFinder />
 
 export default FindMatchup
